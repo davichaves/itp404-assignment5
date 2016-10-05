e78cba9d4d39d0baad285271f0221073730aa6af
Davi Chaves <dchaves@usc.edu>
2016-10-05 01:53:34 -0700

    now really pt 3 done

--- app/components/song-item.js
+++ app/components/song-item.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   songIsPopular: function() {
     return this.song.playCount>20;
+  }.property(),
+
+  songIsExpensive: function() {
+    return this.song.price>1;
   }.property()
 });
