abe2831ca3224851853893c9f06110735197731b
Davi Chaves <dchaves@usc.edu>
2016-10-04 23:58:45 -0700

    part1 almost done

--- app/routes/artists.js
+++ app/routes/artists.js
@@ -1,8 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel() {
-    this.transitionTo('artists');
+  model: function() {
+    return $.ajax({
+    url: 'http://itp-api.herokuapp.com/api/artists',
+    type: 'get'
+    }).then(function(json){
+      console.log(json);
+      return json.artists.map(function(eachArtist){
+        var cleanArrayOfArtists = {
+          id: eachArtist.id,
+          name: eachArtist.name
+        };
+        return cleanArrayOfArtists;
+      });
+    });
   }
 });
 
